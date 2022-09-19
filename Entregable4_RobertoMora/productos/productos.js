24e20821f71853083e1b2556d7ec45106040a9a7
SaintBerry <robertom.95@hotmail.com>
2022-09-19 10:47:25 -0500

    Se realiza cambio solicitado.

---
+++ Entregable4_RobertoMora/productos/productos.js
@@ -0,0 +1,28 @@
+let productos = [
+    {
+      id: 1,
+      title: "Jabon",
+      price: "35",
+      url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdefinicion.de%2Fwp-content%2Fuploads%2F2009%2F06%2Fproducto.png&imgrefurl=https%3A%2F%2Fdefinicion.de%2Fproducto%2F&tbnid=8bnIkwWbu7uGCM&vet=12ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ..i&docid=kyhCT_35T45iEM&w=640&h=457&q=producto&ved=2ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ"
+    },
+    {
+      id: 2,
+      title: "Esponja",
+      price: "35",
+      url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdefinicion.de%2Fwp-content%2Fuploads%2F2009%2F06%2Fproducto.png&imgrefurl=https%3A%2F%2Fdefinicion.de%2Fproducto%2F&tbnid=8bnIkwWbu7uGCM&vet=12ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ..i&docid=kyhCT_35T45iEM&w=640&h=457&q=producto&ved=2ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ"
+    },
+    {
+      id: 3,
+      title: "Escoba",
+      price: "75",
+      url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdefinicion.de%2Fwp-content%2Fuploads%2F2009%2F06%2Fproducto.png&imgrefurl=https%3A%2F%2Fdefinicion.de%2Fproducto%2F&tbnid=8bnIkwWbu7uGCM&vet=12ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ..i&docid=kyhCT_35T45iEM&w=640&h=457&q=producto&ved=2ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ"
+    },
+    {
+      id: 4,
+      title: "Trapeador",
+      price: "85",
+      url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdefinicion.de%2Fwp-content%2Fuploads%2F2009%2F06%2Fproducto.png&imgrefurl=https%3A%2F%2Fdefinicion.de%2Fproducto%2F&tbnid=8bnIkwWbu7uGCM&vet=12ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ..i&docid=kyhCT_35T45iEM&w=640&h=457&q=producto&ved=2ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ"
+    }
+  ];
+
+  module.exports = productos;
--- Entregable4_RobertoMora/routers/productos.js
+++ Entregable4_RobertoMora/routers/productos.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { Router } = express;
+let productos = require("../productos/productos")
 
 const router = Router();
 
@@ -10,33 +11,6 @@ const STATUS_CODE = {
   NOT_FOUND: 404,
   BAD_REQUEST: 400,
 }
-
-let productos = [
-  {
-    id: 1,
-    title: "Jabon",
-    price: "35",
-    url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdefinicion.de%2Fwp-content%2Fuploads%2F2009%2F06%2Fproducto.png&imgrefurl=https%3A%2F%2Fdefinicion.de%2Fproducto%2F&tbnid=8bnIkwWbu7uGCM&vet=12ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ..i&docid=kyhCT_35T45iEM&w=640&h=457&q=producto&ved=2ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ"
-  },
-  {
-    id: 2,
-    title: "Esponja",
-    price: "35",
-    url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdefinicion.de%2Fwp-content%2Fuploads%2F2009%2F06%2Fproducto.png&imgrefurl=https%3A%2F%2Fdefinicion.de%2Fproducto%2F&tbnid=8bnIkwWbu7uGCM&vet=12ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ..i&docid=kyhCT_35T45iEM&w=640&h=457&q=producto&ved=2ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ"
-  },
-  {
-    id: 3,
-    title: "Escoba",
-    price: "75",
-    url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdefinicion.de%2Fwp-content%2Fuploads%2F2009%2F06%2Fproducto.png&imgrefurl=https%3A%2F%2Fdefinicion.de%2Fproducto%2F&tbnid=8bnIkwWbu7uGCM&vet=12ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ..i&docid=kyhCT_35T45iEM&w=640&h=457&q=producto&ved=2ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ"
-  },
-  {
-    id: 4,
-    title: "Trapeador",
-    price: "85",
-    url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdefinicion.de%2Fwp-content%2Fuploads%2F2009%2F06%2Fproducto.png&imgrefurl=https%3A%2F%2Fdefinicion.de%2Fproducto%2F&tbnid=8bnIkwWbu7uGCM&vet=12ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ..i&docid=kyhCT_35T45iEM&w=640&h=457&q=producto&ved=2ahUKEwju5OHdk_n5AhUGnWoFHUukAnsQMygBegUIARC-AQ"
-  },
-];
 
 router.post('/productos', (req, res) => {
   let { body : data } = req
