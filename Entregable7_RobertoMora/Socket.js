fc383696ae37456c04f4acae105f2ce3cd14b360
SaintBerry <robertomorab.19@gmail.com>
2022-10-17 20:13:31 -0500

    Historial Completo

--- Entregable7_RobertoMora/Socket.js
+++ Entregable7_RobertoMora/Socket.js
@@ -1,6 +1,8 @@
 import {Server} from "socket.io";
 import {productos} from "./db/productos.js"
+import * as fs from "fs";
 
+let path= "./historial/historial.txt"
 let messages = [
   {
     email: 'Coder House',
@@ -8,6 +10,48 @@ let messages = [
     message: 'Bienvenidos'
   },
 ]
+
+function saveMessage(objects) {
+  let file, data, contador;
+  try {
+    file = fs.readFileSync(path);
+    try {
+      data = JSON.parse(file);
+      contador = data.length;
+      objects.id = contador;
+      data.push(objects);
+      historial(path, data);
+    } catch (e) {
+      console.log(e);
+    }
+  } catch (e) {
+    fs.writeFileSync(path, "[\n\t");
+    objects.id = 1;
+    fs.appendFileSync(path, `{\n\t\t"email": "${objects.email}",\n\t`);
+    fs.appendFileSync(path, `\t"date": "${objects.date}",\n\t`);
+    fs.appendFileSync(path, `\t"message": "${objects.message}",\n\t`);
+    fs.appendFileSync(path, `\t"id": ${objects.id}\n\t`);
+    fs.appendFileSync(path, `}\n`);
+    fs.appendFileSync(path, "]");;
+  }
+}
+
+function historial(path, data) {
+  fs.writeFileSync(path, "[\n\t");
+  data.map((object, id) => {
+    object.id = id + 1;
+    fs.appendFileSync(path, `\t{\n\t\t"email": "${object.email}",\n\t`);
+    fs.appendFileSync(path, `\t"date": "${object.date}",\n\t`);
+    fs.appendFileSync(path, `\t"message": "${object.message}",\n\t`);
+    fs.appendFileSync(path, `\t"id": ${object.id}\n\t`);
+    if (id + 1 !== data.length) {
+      fs.appendFileSync(path, `},\n`);
+    } else {
+      fs.appendFileSync(path, `}\n`);
+    }
+  });
+  fs.appendFileSync(path, "]");
+}
 
 let io;
 
@@ -45,13 +89,16 @@ export default class Socket {
             }
           )
           io.emit('notification-message', {
-            
               email: data.email,
               date: new Date().toLocaleString(),
               message: data.message
             }
           )
+          data={...data, date: new Date().toLocaleString()}
+          saveMessage(data)
         })
+
+
         clienteSocket.on('disconection', () => {
           console.log('Se desconecto el cliente con el id', clienteSocket.id)
         })
